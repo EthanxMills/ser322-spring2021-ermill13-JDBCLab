66d420527560549b499657fc19fa633f03e05408
EthanxMills <emills.formal@gmail.com>
2021-04-19 20:33:51 -0600

    Write Query 2

--- src/ser322/ser322JDBCLab.java
+++ src/ser322/ser322JDBCLab.java
@@ -6,8 +6,14 @@ class ser322JDBCLab{
     
     public static void main(String[] args){
         try{
-        Connection con = null;
-        String url = args[0], user = args[1], pwd = args[2], driver = args[3], query = args[4];
+
+            Connection con = null;
+            String url = args[0]; 
+            String user = args[1];
+            String pwd = args[2];
+            String driver = args[3];
+            String query = args[4];
+
             try{
 
                 Class.forName(driver);
@@ -32,7 +38,7 @@ class ser322JDBCLab{
             exception.printStackTrace();
         }
     }
-    
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     protected static void query1(Connection con){//creation for query1, assuming args are given as in the doc I can pass the connect
         try{
             Statement stmnt = con.createStatement();//Create Statement
@@ -41,7 +47,7 @@ class ser322JDBCLab{
             ResultSet rSet = stmnt.executeQuery(queryHolder);//Execute whatever is in the query string and store in rSet
 
             while(rSet.next()){
-                System.out.println(rSet.getInt(1) + " | " + rSet.getString(2) + " | " + rSet.getString(3));//Adding space with border to allign the columns 
+                System.out.println(rSet.getInt(1) + " | " + rSet.getString(2) + " | " + rSet.getString(3));//Adding space with border to allign the columns
             }
             con.close();
         }catch(SQLException exception){
@@ -50,6 +56,20 @@ class ser322JDBCLab{
     }
 
     protected static void query2(Connection con){
+        try{
+            
+            String prepStat = "select dept.DNAME,customer.NAME,product.PRICE from product,customer,dept where product.MADE_BY=? and product.PRODID=customer.PID;";
+            PreparedStatement pStat=con.prepareStatement(prepStat);
+
+            ResultSet rSet = pStat.executeQuery();//Execute whatever is in the query string and store in rSet
+
+            while(rSet.next()){
+                System.out.println(rSet.getInt(1) + " | " + rSet.getString(2) + " | " + rSet.getString(3));//Adding space with border to allign the columns
+            }
+            con.close();
+        }catch(SQLException exception){
+            exception.printStackTrace();
+        }
 
     }
 }
