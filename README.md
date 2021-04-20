ee2b7614f730e352d1c06c99e5535e7deb932d7d
EthanxMills <emills.formal@gmail.com>
2021-04-19 19:57:48 -0600

    Activity 1a
    Made the Query, Driver Connection, Executed a statement which should be stored in rSet

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ser322-spring2021-ermill13-JDBCLab
 ### Repo For Ethan Miller's JDBC Lab For SER 322
+
+## Resources used For Assignment:
+
+- [Driver Download](https://dev.mysql.com/downloads/connector/j/)
+- [Connecting Driver to My Query Method](https://stackoverflow.com/questions/15039265/what-exactly-does-this-do-class-fornamecom-mysql-jdbc-driver-newinstance/15039333)
+- [Java Connection Establishing](https://docs.oracle.com/javase/tutorial/jdbc/basics/connecting.html)
+- [Iterating Through ResultSet](http://tutorials.jenkov.com/jdbc/resultset.html#:~:text=To%20iterate%20the%20ResultSet%20you,and%20you%20can%20no%20longer.)
+
--- src/ser322/ser322JDBCLab.java
+++ src/ser322/ser322JDBCLab.java
@@ -1,6 +1,8 @@
 package src.ser322;
 import java.sql.*;
 
+import javax.naming.spi.DirStateFactory.Result;
+
 
 class ser322JDBCLab{
     
@@ -10,6 +12,15 @@ class ser322JDBCLab{
         try{
 
             Class.forName("com.mysql.jdbc.Driver");//Line to Load the Driver from MySQL Community
+
+            Connection con=DriverManager.getConnection(
+            "jdbc:mysql://127.0.0.1:3306/jdbclab","root","romanmiller"); //The LocalInstance made by Workbench was all weird so I made a new one, should connect
+            
+            Statement stmnt = con.createStatement();//Create Statement
+
+            String queryHolder = "SELECT empno,ename,dept.DNAME from emp,dept WHERE emp.deptno=dept.DEPTNO";
+            ResultSet rSet = stmnt.executeQuery(queryHolder);//Execute whatever is in the query string and store in rSet
+
 
         }catch(Exception exception){
             System.out.println(exception);
