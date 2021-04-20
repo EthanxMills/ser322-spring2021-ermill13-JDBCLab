5d5863bcf17f6335c21c91a61cde72f2869932e7
EthanxMills <emills.formal@gmail.com>
2021-04-19 20:08:46 -0600

    Reformat 1.1

--- src/ser322/ser322JDBCLab.java
+++ src/ser322/ser322JDBCLab.java
@@ -6,13 +6,11 @@ class ser322JDBCLab{
     
     
     public void query1(){//creation for query1, I downloaded the JDBC Driver From SQL Community so hope this works
-
         try{
-
             Class.forName("com.mysql.jdbc.Driver");//Line to Load the Driver from MySQL Community
 
-            Connection con=DriverManager.getConnection(
-            "jdbc:mysql://127.0.0.1:3306/jdbclab","root","romanmiller"); //The LocalInstance made by Workbench was all weird so I made a new one, should connect
+            Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/jdbclab","root","romanmiller"); 
+            //The LocalInstance made by Workbench was all weird so I made a new one, should connect
             
             Statement stmnt = con.createStatement();//Create Statement
 
@@ -22,11 +20,12 @@ class ser322JDBCLab{
             while(rSet.next()){
                 System.out.println(rSet.getInt(1) + " | " + rSet.getString(2) + " | " + rSet.getString(3));//Adding space with border to allign the columns 
             }
-
-
         }catch(Exception exception){
             System.out.println(exception);
         }
+    }
 
+    public void query2(){
+        
     }
 }
